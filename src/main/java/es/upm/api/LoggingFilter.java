bdc3919725b8575fafc59e621aad1a4ffc9e2c98
JBernal <j.bernal@upm.es>
2025-07-25 04:38:10 +0200

    fixed LoggingFilter

--- src/main/java/es/upm/api/LoggingFilter.java
+++ src/main/java/es/upm/api/LoggingFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -20,7 +21,7 @@ import java.util.stream.Collectors;
 @Log4j2
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-//@Profile({"dev"})
+@Profile({"dev"})
 public class LoggingFilter extends OncePerRequestFilter {
 
 
